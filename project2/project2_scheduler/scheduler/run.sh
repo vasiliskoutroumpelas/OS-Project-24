9855feafcfd65af4a051a58bbbaa1f038d5d939a
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-01-15 06:26:05 +0200

    uncommented available options

--- project2/project2_scheduler/scheduler/run.sh
+++ project2/project2_scheduler/scheduler/run.sh
@@ -1,8 +1,8 @@
 ./scheduler FCFS homogeneous.txt
-# ./scheduler RR 1000 homogeneous.txt
+./scheduler RR 1000 homogeneous.txt
 
 ./scheduler FCFS reverse.txt
-# ./scheduler RR 1000 reverse.txt
+./scheduler RR 1000 reverse.txt
 
 # ./scheduler_io FCFS mixed.txt
 # ./scheduler_io RR 1000 mixed.txt
