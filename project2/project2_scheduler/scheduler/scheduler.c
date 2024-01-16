b4e9890cc61955b8c657395a1bcc436d534703e7
vasiliskoutrou03@gmail.com <vasiliskoutrou03@gmail.com>
2024-01-16 07:56:04 +0200

    clean up and comments

--- project2/project2_scheduler/scheduler/scheduler.c
+++ project2/project2_scheduler/scheduler/scheduler.c
@@ -12,6 +12,7 @@ Filippos Minopetros, 1093431
 #include <unistd.h>
 #include <signal.h>
 
+// structure that describes a process
 typedef struct node {
     char* name;
 	pid_t pid;
@@ -79,17 +80,19 @@ Process* pop_first(Process** head)
 	return temp;
 }
 
+// display function, prints the details of all the process in the list 
 void print_list(Process* list) {
     Process* current = list;
 
     while (current != NULL) {
-        printf("Name: %s, PID: %d, State: %s, Entry: %ld\n",
-               current->name, current->pid, current->state, (long)current->entry);
+        printf("Name: %s, PID: %d, State: %s",
+               current->name, current->pid, current->state);
         current = current->next;
     }
 	free(current);
 }
 
+// display function, prints the details of a single process
 void print_process(Process* process) {
 	printf("Name: %s, PID: %d, State: %s\n",
 			process->name, process->pid, process->state);
@@ -103,27 +106,31 @@ void update_state(Process* process, const char* state) {
     strcpy(process->state, state);
 }
 
+// function that starts a process via forking and executing the child process
 void start_process(Process* process) 
 {
 	pid_t pid = fork();
-	if (pid == 0)
+	if (pid == 0) // child process
 	{
-		// printf("%s\n", process->name);
 		execl(process->name, process->name, NULL);
 		perror("execl fail");
 	}
-	else
+	else // parent process
 	{
 		process->pid = pid;
 	}
 }
 
+// global variables to be used by the scheduler
+Process *list, *current_process;
 
-int child_finished;
+// global variable which acts as a flag for the status of a process in execution
+int child_finished=0;
 void handle_child_finish(int sigid) {
 	child_finished = 1;
 }
 
+// helper function that converts milliseconds to seconds and nanoseconds to be used by nanosleep
 void sleep_milliseconds(unsigned int milliseconds) {
     struct timespec ts;
     ts.tv_sec = milliseconds / 1000;
@@ -132,16 +139,17 @@ void sleep_milliseconds(unsigned int milliseconds) {
 }
 
 int main(int argc,char **argv)
-{	
+{
+	// lines of code that secure the user is executing the program the right way	
 	if (argc < 3 || argc > 4)
 	{
         printf("Usage: %s <policy> [<quantum>] <input_filename>\n", argv[0]);
         exit(1);
     }
 
+	// variables
 	char *policy = argv[1];
 	int quantum = 1000; 
-
 	FILE* file; 
 
 	if (argc == 4 && strcmp(policy, "RR")==0)
@@ -172,63 +180,61 @@ int main(int argc,char **argv)
         return 1;
     }
 
-	Process *list;
+	// insert all processes from the file to the list
 	char name[100];
-	
 	while (fscanf(file, "%s\n", name) != EOF)
 	{
 		insert_end(&list, name, -1, "NEW", time(NULL));
 	}
 	
 	fclose(file);
-	// print_list(list);
 
+	// defining action
 	struct sigaction sact;
 	sact.sa_handler = handle_child_finish;
 	sact.sa_flags = SA_NOCLDSTOP;
-	
 	
 	if (sigaction (SIGCHLD, &sact, NULL) < 0)
 		perror("could not set action for SIGINT\n");
 	
 
-
+	// start of scheduler
 	time_t start = time(NULL);
 	while (list != NULL)
 	{
-		Process* process = pop_first(&list);
+		current_process = pop_first(&list);
 		
-		if (strcmp(process->state, "NEW")==0)
+		if (strcmp(current_process->state, "NEW")==0)
 		{
-			start_process(process);
-			update_state(process, "RUNNING");
+			start_process(current_process);
+			update_state(current_process, "RUNNING");
 			printf("\n");
-			print_process(process);
+			print_process(current_process);
 		}
 		else
 		{
-			kill(process->pid, SIGCONT);
-			update_state(process, "RUNNING");
-			print_process(process);
+			kill(current_process->pid, SIGCONT);
+			update_state(current_process, "RUNNING");
+			print_process(current_process);
 		}
 		
 		sleep_milliseconds(quantum);
 		
 		if(child_finished)
 		{
-			update_state(process, "EXITED");
-			process->entry = time(NULL)-start;
-			print_process(process);
-			print_time_since_entry(process);
+			update_state(current_process, "EXITED");
+			current_process->entry = time(NULL)-start;
+			print_process(current_process);
+			print_time_since_entry(current_process);
 			child_finished = 0;
 		}
 		else
 		{
-			kill(process->pid, SIGSTOP);
-			update_state(process, "STOPPED");
-			print_process(process);
+			kill(current_process->pid, SIGSTOP);
+			update_state(current_process, "STOPPED");
+			print_process(current_process);
 			
-			insert_end(&list, process->name, process->pid, "STOPPED", process->entry);
+			insert_end(&list, current_process->name, current_process->pid, "STOPPED", current_process->entry);
 		}
 	}
 	time_t end = time(NULL);
